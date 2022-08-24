2d930d9d5623d27fbdbdb88488c81ee1dc2f4e77
Evilcarry <benjhithaxx@gmail.com>
2022-08-25 11:07:26 +1200

    pushing the new features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import {
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
-//import NotFoundPage from "./pages/404";
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
     return (
@@ -19,14 +19,15 @@ const App = () =>  {
             <ul className="header">
             <li><NavLink exact to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                <li><NavLink to = "/Submit-Article">Submit an Article</NavLink></li>
             </ul>
         </div>
           <div className="content">
             <Routes>
               <Route exact path="/" element={<Home/>}/>
               <Route  path="/SEPractice" element={<SEPractice/>}/>
-              <Route  path="/SubmitArticle" elelement={<SubmitArticle/>}/>
+              <Route  path="/Submit-Article" element={<SubmitArticle/>}/>
+              <Route path="*" element={<NotFoundPage/>} replace />
             </Routes>
           </div>
         </Router>
